c7262c0148f3e6d2831ad40c486bca34794081b2
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-04-11 09:58:53 +0700

    first commit

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -2,11 +2,13 @@ export class FizzBuzz {
     message: string;
 
     constructor(n: number) {
-        if (n % 3 == 0 && n % 5 == 0) {
+        let isBuzz = n % 3;
+        let isFizz = n % 5;
+        if (isBuzz == 0 && isFizz == 0) {
             this.message = 'FizzBuzz';
-        } else if (n % 5 == 0) {
+        } else if (isFizz == 0) {
             this.message = 'Fizz';
-        } else if(n % 3 == 0) {
+        } else if (isBuzz == 0) {
             this.message = 'Buzz';
         } else {
             this.message = n + ''
